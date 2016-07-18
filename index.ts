c503cf6934af0185b6e15d15e721f73cf194f19c
tyler-morrison <tyler.morrison@thehartford.com>
2016-07-18 15:52:44 -0400

    Cleaned up  and added Typescript support

---
+++ index.ts
@@ -0,0 +1,16 @@
+/**
+ * External Dependencies
+ */
+
+
+/**
+ * Internal Dependencies
+ */
+
+let trueVar = true;
+
+if (trueVar) {
+  let testVar = 1;
+}
+
+console.log(trueVar);
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
-'use strict';
+"use strict";
 /**
  * External Dependencies
  */
-const path = require('path'),
-  merge = require('webpack-merge'),
-  validate = require('webpack-validator');
+const path = require("path"),
+  merge = require("webpack-merge"),
+  validate = require("webpack-validator");
 
 /**
  * Internal Dependencies
  */
-const parts = require('require-all')(__dirname + '/lib');
+const parts = require("require-all")(__dirname + "/lib");
 
 const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
-  client: path.join(__dirname, 'client'),
-  server: path.join(__dirname, 'server'),
+  client: path.join(__dirname, "client"),
+  server: path.join(__dirname, "server"),
   style: [
-    path.join(__dirname, 'assets', 'style.scss')
+    path.join(__dirname, "assets", "style.scss")
   ],
-  build: path.join(__dirname, 'build'),
-  test: path.join(__dirname, 'tests')
+  build: path.join(__dirname, "build"),
+  test: path.join(__dirname, "tests")
 };
 
 process.env.BABEL_ENV = TARGET;
@@ -28,45 +28,47 @@ process.env.BABEL_ENV = TARGET;
 const common = merge(
   {
     // Entry accepts a path or an object of entries.
-    // We'll be using the latter form given it's
+    // We’ll be using the latter form given it’s
     // convenient with more complex configurations.
     entry: {
-      app: 'index.js'
+      app: "index.ts"
     },
     output: {
       path: PATHS.build,
-      filename: '[name].js'
+      filename: "[name].js"
       // TODO: Set publicPath to match your GitHub project name
-      // E.g., '/kanban-demo/'. Webpack will alter asset paths
+      // E.g., `/kanban-demo/`. Webpack will alter asset paths
       // based on this. You can even use an absolute path here
       // or even point to a CDN.
-      //publicPath: ''
+      //publicPath: ""
     },
     resolve: {
-      // Important! Do not remove ''. If you do, imports without an extension won't work anymore!
-      extensions: ['', '.js', '.jsx'],
+      // Important! Do not remove "". If you do, imports without an extension won"t work anymore!
+      extensions: ["", ".js", ".ts", ".tsx"],
       // Include both `client` and `server` to root so that all modules resolve.
       root: [ PATHS.server, PATHS.client, __dirname],
-      modulesDirectories: [ 'node_modules' ]
+      modulesDirectories: [ "node_modules" ]
     }
-  }
+  },
+  parts.loadTSX()
 );
 
-let config;
+let webpackConfig;
 
 // Detect how npm is run and branch based on that
 switch (TARGET) {
-  case 'build':
+  case "build":
   default:
-    config = merge(
+    webpackConfig = merge(
       common,
       {
-        devtool: 'eval-source-map',
+        devtool: "eval-source-map",
       },
+      // TODO: Set dev ? true | false based on env variable
       parts.npmInstall()
     );
 }
 
-module.exports = validate(config, {
+module.exports = validate(webpackConfig, {
   quiet: true
 });
